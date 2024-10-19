0210fa44a87ad246a281cbd1a1bbae0526fec09a
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-10-20 00:50:24 +0600

    button toggle

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags, population, area } = country;
+  const { name, flags, population, area, cca3 } = country;
+
+  const [isVisited, setIsVisited] = useState(false);
+
+  const handleClick = () => {
+    setIsVisited(!isVisited);
+  };
   return (
-    <div className="country">
+    <div className={`country ${isVisited ? "visited" : ""}`}>
       <p>{name.common}</p>
       <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
+      <p>Code: {cca3}</p>
+      <button onClick={handleClick}>
+        {isVisited ? "Visited" : "Plan for Goining"}
+      </button>
     </div>
   );
 };
