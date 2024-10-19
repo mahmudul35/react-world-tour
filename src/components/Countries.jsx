c06d7377c2a1d9e6ac7a3b32b4191db7c2f4b44a
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-10-20 00:15:40 +0600

    make card in 3 column

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
+import "./Countries.css";
 import Country from "./Country";
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
   return (
     <div>
       <p> Countries:{countries.length}</p>
-      <div>
+      <div className="country-container">
         {countries.map((country) => (
-          <Country country={country} />
+          <Country key={country.cca2} country={country} />
         ))}
       </div>
     </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area } = country;
   return (
     <div className="country">
-      <p>Name:{name.common}</p>
+      <p>{name.common}</p>
       <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
     </div>
   );
 };
