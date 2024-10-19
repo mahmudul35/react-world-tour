47250c26d0fc9e65b6c592d4071f4c15e3df8a3a
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-10-20 01:12:41 +0600

    listing visited countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,6 +3,14 @@ import "./Countries.css";
 import Country from "./Country";
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitCountries = (country) => {
+    // console.log("visited omok country");
+
+    const addCountry = [...visitedCountries, country];
+    setVisitedCountries(addCountry);
+  };
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -13,9 +21,21 @@ export default function Countries() {
   return (
     <div>
       <p> Countries:{countries.length}</p>
+      <div>
+        <h3>Visited Countries:{visitedCountries.length}</h3>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
       <div className="country-container">
         {countries.map((country) => (
-          <Country key={country.cca2} country={country} />
+          <Country
+            key={country.cca2}
+            country={country}
+            handleVisitCountries={handleVisitCountries}
+          />
         ))}
       </div>
     </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitCountries }) => {
   const { name, flags, population, area, cca3 } = country;
 
   const [isVisited, setIsVisited] = useState(false);
@@ -15,6 +15,10 @@ const Country = ({ country }) => {
       <p>Population: {population}</p>
       <p>Area: {area}</p>
       <p>Code: {cca3}</p>
+      <button onClick={() => handleVisitCountries(country)}>
+        Mark Visited
+      </button>{" "}
+      <br /> <br />
       <button onClick={handleClick}>
         {isVisited ? "Visited" : "Plan for Goining"}
       </button>
